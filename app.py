a834dd35d90806f0eca2cc13431e50aa1667a2c0
vinaybailwal00-glitch <vinaybailwal00@gmail.com>
2026-06-17 03:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("🤖 AI Customer Support Assistant")
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-pro")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
